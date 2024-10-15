0273f1e34ab7d941fe9cdce786844f091ff74988
alan2325 <alalkumar06@gmail.com>
2024-10-15 09:37:09 +0530

    pattern

--- 0ct_14/pattern2.py
+++ 0ct_14/pattern2.py
@@ -1,9 +1,18 @@
-n = 3  
+n = 65 
 
-for i in range(n):
-    for j in range(n):
+for i in range(3):
+    a=1
+    for j in range(3):
         if i == j:
-            print(1, end=" ")
+            print(a, end=" ")
         else:
-            print(chr(65 + j), end=" ")
-    print()  
+            print(chr(n), end=" ")
+    print()
+    n+=1
+
+
+   
+
+    ##1 A A
+    ##B 1 B 
+    ##C C 1
